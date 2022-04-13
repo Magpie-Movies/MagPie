896eb838d151890fc43ecfa55e659dd5ccbdb447
MartinRenteriaVerizon <martin.renteria@verizon.com>
2022-04-13 10:29:08 -0400

    Adding basic auth and OAuth logic, adding frontend files and routes

--- api/app.js
+++ api/app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require("express");
 //require basicAuth
 const basicAuth = require("express-basic-auth");
@@ -7,7 +9,7 @@ const bcrypt = require("bcrypt");
 const saltRounds = 2;
 
 //require models and associations
-const initialiseDb = require('./index.js');
+const initialiseDb = require("./index.js");
 initialiseDb();
 
 const { User } = require("./models/user");
@@ -22,26 +24,31 @@ const { Country } = require("./models/country.js");
 const app = express();
 
 // serve static assets from the public/ folder
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 // specify out request bodies are json
 app.use(express.json());
 // support the parsing of incoming requests with urlencoded payloads (e.g. form POST)
 app.use(express.urlencoded({ extended: false }));
 
-var jwt = require("express-jwt");
+var jwtC = require("express-jwt");
 var jwks = require("jwks-rsa");
+const jwt = require('jsonwebtoken')
+// const authTokens = {};
+
+// const generateAuthToken = () => {
+//   return bcrypt.hashSync(Math.random().toString(), bcrypt.genSaltSync(8));
+// };
 
 // routes go here
-app.get("/", (req, res) => {
+app.get("/", (req, res) => {});
+
+app.get("/signup", (req, res) => {
+  res.sendFile(__dirname + "/public/signup.html");
 });
 
-app.get('/signup', (req, res) => {
-  res.sendFile(__dirname + '/public/signup.html');
-})
-
-app.get('/signin', (req, res) => {
-  res.sendFile(__dirname + '/public/signin.html');
-})
+app.get("/signin", (req, res) => {
+  res.sendFile(__dirname + "/public/signin.html");
+});
 
 app.post("/signup", async (req, res) => {
   const username = req.body.username;
@@ -53,7 +60,46 @@ app.post("/signup", async (req, res) => {
   });
 });
 
-// var jwtCheck = jwt({
+app.post("/signin", async (req, res) => {
+  // Authenticate User
+  const username = req.body.username;
+  const user = { name: username };
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "1h",
+  });
+  res.json({ accessToken: accessToken });
+});
+
+// app.post(
+//   "/signin",
+//   async (req, res) => {
+//     const username = req.body.username;
+//     const user = await User.findOne({ where: { username: username } });
+//     if (!user) {
+//       res.status(401).send("User not found");
+//     } else {
+//       bcrypt.compare(req.body.password, user.password, function (err, result) {
+//         if (user) {
+//           const authToken = generateAuthToken();
+//           console.log("authTokens", authTokens);
+
+//           // Store authentication token
+//           authTokens[authToken] = user;
+
+//           // Setting the auth token in cookies
+//           res.cookie("AuthToken", authToken);
+
+//           // Redirect user to the protected page
+//           res.redirect("/users");
+//         } else {
+//           res.status(401).send("Password incorrect");
+//         }
+//       });
+//     }
+//   }
+// );
+
+// var jwtCheck = jwtC({
 //   secret: jwks.expressJwtSecret({
 //     cache: true,
 //     rateLimit: true,
@@ -70,6 +116,117 @@ app.post("/signup", async (req, res) => {
 // app.get("/authorized", function (req, res) {
 //   res.send("Secured Resource");
 // });
+
+app.get("/users", authenticateToken, async (req, res) => {
+  const Allusers = await User.findAll();
+  res.json(Allusers);
+});
+
+app.get("/users/:id", authenticateToken, async (req, res) => {
+  const user = await User.findOne({ where: { id: req.params.id } });
+  res.json(user);
+});
+
+//movie route
+//add movies
+app.post("/movies", async (req, res) => {
+  let newMovie = await Movie.create(req.body);
+  res.json({ newMovie });
+});
+
+//get all movies
+app.get("/api/movies", authenticateToken, async (req, res) => {
+  const allMovies = await Movie.findAll();
+  res.json({ allMovies });
+});
+
+//get movies by id
+app.get("/api/movies:id", authenticateToken, async (req, res) => {
+  const moviePK = await Movie.findByPk(req.params.id);
+  res.json({ moviePK });
+});
+
+//category
+//add category entries to db
+app.post("/api/categories", async (req, res) => {
+  const newCategory = await Category.create(req.body);
+  res.json({ newCategory });
+});
+
+app.get("/api/categories", authenticateToken, async (req, res) => {
+  const allCategories = await Category.findAll();
+  res.json({ allCategories });
+});
+
+//get category
+app.get("/api/categories:name", authenticateToken, async (req, res) => {
+  const getCategory = await Category.findAll({
+    where: {
+      name: req.params.name,
+    },
+  });
+  res.json({ getCategory });
+});
+
+//get category by id
+app.put("/api/categories:id", async (req, res) => {
+  const updateCategory = await Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  });
+  res.json({ updateCategory });
+});
+
+//delete categories
+app.delete("/api/categories:id", async (req, res) => {
+  await Category.destroy({ where: { id: req.params.id } });
+  res.send("Deleted!");
+});
+
+//country
+//add country entries to the db
+app.post("/api/countries", async (req, res) => {
+  let newCountry = await Country.create(req.bod);
+  res.json({ newCountry });
+});
+
+//get country by id
+app.get("/api/countries:id", authenticateToken, async (req, res) => {
+  let getCountry = await Country.findByPk(req.params.id);
+  res.json({ getCountry });
+});
+
+//get country by name
+app.get("/api/countries:name", authenticateToken, async (req, res) => {
+  let getCountry = await Country.findAll({
+    where: {
+      country_name: req.params.name,
+    },
+  });
+  res.json({ getCountry });
+});
+
+//update Country
+app.put("/api/countries:id", async (req, res) => {
+  const updateCountry = await Country.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  });
+  res.json(updateCountry);
+});
+
+//delete country
+app.delete("/api/countries:id", async (req, res) => {
+  await Country.destroy({ where: { id: req.params.id } });
+  res.send("Deleted!");
+});
+
+app.post("/api/person", async (req, res) => {
+  let newPerson = await Cast_Crew.create(req.body);
+  res.json(newPerson);
+});
 
 app.use(
   basicAuth({
@@ -92,122 +249,22 @@ async function dbAuthorizer(username, password, callback) {
     console.log("isValid", isValid);
   } catch (err) {
     //if authorize fails, log error
-    console.log("Error ", err);
+    res.send('401 Not Authorized');
     callback(null, false);
   }
 }
 
-app.get("/users", async (req, res) => {
-  const Allusers = await User.findAll();
-  res.json(Allusers);
-});
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
 
-app.get("/users:id", async (req, res) => {
-  const user = await User.findOne({ where: { id: req.params.id } });
-  res.json(user);
-});
-
-//movie route 
-//add movies
-app.post("/movies", async(req, res) => {
-  let newMovie = await Movie.create(req.body);
-  res.json({newMovie})
-});
-
-//get all movies
-app.get("/api/movies", async (req, res) => {
-  const allMovies = await Movie.findAll();
-  res.json({allMovies});
-});
-
-//get movies by id
-app.get("/api/movies:id", async (req, res) => {
-  const moviePK = await Movie.findByPk(req.params.id);
-  res.json({moviePK});
-});
-
-//category 
-//add category entries to db
-app.post("/api/categories", async(req, res) => {
-  const newCategory = await Category.create(req.body);
-  res.json({newCategory});
-});
-
-app.get("/api/categories", async(req, res) => {
-  const allCategories = await Category.findAll();
-  res.json({allCategories});
-});
-
-//get category
-app.get("/api/categories:name", async(req, res) =>{
-  const getCategory = await Category.findAll({
-    where: {
-      name: req.params.name
-    }
-  })
-  res.json({getCategory});
-});
-
-//get category by id
-app.put("/api/categories:id", async(req, res) => {
-  const updateCategory = await Category.update(req.body, {
-    where: { 
-      id: req.params.id
-    }
-  })
-  res.json({updateCategory})
-})
-
-//delete categories
-app.delete('/api/categories:id', async(req, res)=> {
-  await Category.destroy({where: {id: req.params.id}});
-  res.send('Deleted!')
-})
-
-//country
-//add country entries to the db
-app.post("/api/countries", async(req, res) =>{
-  let newCountry = await Country.create(req.bod);
-  res.json({newCountry})
-})
-
-//get country by id
-app.get("/api/countries:id", async(req, res) =>{
-  let getCountry = await Country.findByPk(req.params.id);
-  res.json({getCountry})
-})
-
-//get country by name
-app.get("/api/countries:name", async(req, res) => {
-  let getCountry = await Country.findAll({
-    where: {
-      country_name: req.params.name
-    }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
   });
-  res.json({getCountry})
-})
-
-//update Country
-app.put("/api/countries:id", async(req, res) => {
-  const updateCountry = await Country.update(req.body, {
-    where: {
-      id: req.params.id
-    }
-  });
-  res.json(updateCountry)
-})
-
-//delete country
-app.delete("/api/countries:id", async(req, res) => {
-  await Country.destroy({where: {id: req.params.id}});
-  res.send('Deleted!')
-})
-
-app.post("/api/person", async(req, res) => {
-  let newPerson = await Cast_Crew.create(req.body);
-  res.json(newPerson);
-});
-
+}
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
