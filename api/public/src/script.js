4feb58c72297cb74998ab638b3d5c46cffe15722
Fox, Lowella <lowella.fox@verizon.com>
2022-04-14 13:41:36 -0500

    Resize textArea height

--- api/public/src/script.js
+++ api/public/src/script.js
@@ -1,9 +1,12 @@
 function runRoute() {
-  const route = document.getElementById("routes").value;
-  fetch(`http://localhost:3000/${route}/`)
+  fetch(`http://localhost:3000/api/movies/search/id/1`)
   .then(response => response.json())
     .then(data => {
       console.log(data)
-    document.getElementById("output").innerHTML = JSON.stringify(data);
+    document.getElementById("output").innerHTML = JSON.stringify(data, null, 4);
+
+    //resize textarea height
+    var txtArea = document.getElementById('output').scrollHeight;
+    document.getElementById('output').setAttribute('style','height:'+txtArea+'px');  
     })
   };
